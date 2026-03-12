3b4e35b67a35b47c17d0aa4072210cc64f48405d
Dang-pig <85467685+Dang-pig@users.noreply.github.com>
2026-03-12 23:43:32 +0700

    Add solution for expected-value-discrete

---
+++ expected-value-discrete/expected-value-discrete.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def expected_value_discrete(x, p):
+    """
+    Returns: float expected value
+    """
+    x = np.asarray(x)
+    p = np.asarray(p)
+    if(p.sum().item() != 1):
+        raise ValueError("Fuck your mom fat")
+    return (x * p).sum().item()
+    pass
